a958ad9adaf07288997fc33b170d48d6bd5b390c
apinchbeck <angie.pinchbeck@gmail.com>
2019-03-28 13:47:56 -0700

    lda complete

--- topic_modelling/modelling.py
+++ topic_modelling/modelling.py
@@ -8,6 +8,9 @@ from lda import LDA
 from docs_preprocessor import DocsPreprocessor
 
 import csv
+from os.path import join as pjoin
+# Set the save path for your own machine
+save_path = "C:\\Users\\angiepin\\Topic Modelling\\results\\"
 
 class Modelling:
     """
@@ -31,12 +34,15 @@ class Modelling:
     def run_algorithms(self, kmin, kmax, kstep, top):
         self.run_lda(kmin, kmax, kstep)
 
-    def run_lda(self, kmin, kmax, kstep):
+    def run_lda(self, kmin, kmax, kstep, n):
         k_values, coherence_values, topic_list =                               \
             self.lda.compute_coherence_values(kmin, kmax, kstep)
         # The next returns the best # of topics and saves the plot
-        best_k = self.plot_coherence(k_values, coherence_values, "lda")
-        self.write_lda_data(topic_list, coherence_values)
+        file_path = save_path + "lda\\"
+        
+        for model_run in range(1, n + 1):
+            best_k = self.plot_coherence(k_values, coherence_values, file_path + str(model_run) + "-")
+            self.write_lda_data(topic_list, coherence_values, file_path + str(model_run) + "-")
 
     def run_nmf(self, kmin, kmax, kstep, top):
         k_values, coherence_values = self.nmf.process_models(
@@ -44,11 +50,11 @@ class Modelling:
         best_k = self.plot_coherence(k_values, coherence_values, "nmf")
         
 
-    def write_nmf_data(self):
+    def write_nmf_data(self, coherence_values):
         """
         """
 
-    def write_lda_data(self, topic_list, coherence_values):
+    def write_lda_data(self, topic_list, coherence_values, file_path):
         """
         """
         i = 0
@@ -66,17 +72,17 @@ class Modelling:
                 t_row.append(top)
                 topwords_data.append(t_row)
             with open(
-                './lda-topic-list-' + str(
+                file_path + 'lda-topic-list-' + str(
                     num_topics) + '.csv', 'w', newline='') as csvFile:
                 writer = csv.writer(csvFile)
                 writer.writerows(topwords_data)
             csvFile.close()
 
-        with open('./coherence-data.csv', 'w', newline='') as csvFile:
+        with open(file_path + 'coherence-data.csv', 'w', newline='') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerows(coherence_data)
 
-    def plot_coherence(self, k_values, coherences, name):
+    def plot_coherence(self, k_values, coherences, file_path):
         fig = plt.figure(figsize=(13,7))
         # create the line plot
         ax = plt.plot( k_values, coherences )
@@ -93,5 +99,5 @@ class Modelling:
             "k=%d" % best_k, xy=(best_k, ymax), xytext=(
                 best_k, ymax), textcoords="offset points", fontsize=16)
         # save the plot
-        plt.savefig(name + "-coherence.jpg")
+        plt.savefig(file_path + "lda-coherence.png")
         return best_k
